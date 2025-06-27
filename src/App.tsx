474b573c5328906b025a2c9d9fb0e5b16ec0797a
Santiago Nickisch <SantiNick28@gmail.com>
2025-06-27 10:48:37 -0300

    Commit inicial para entrega 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Container from './components/Container';
-import SongCard from './components/SongCard'; 
-import { mockSongs } from './data/mockSongs'; 
+import SongCard from './components/SongCard';
+import Sidebar from './components/Sidebar';
+import { mockSongs } from './data/mockSongs';
 
 export type Song = {
   id: string;
@@ -11,26 +13,52 @@ export type Song = {
   imageUrl: string;
 };
 
+export type Playlist = {
+  id: string;
+  name: string;
+  description: string;
+  songs: Song[];
+};
+
 function App() {
+  const [playlists, setPlaylists] = useState<Playlist[]>([]); 
+
+
+  const addPlaylist = (name: string, description: string) => {
+    const newPlaylist: Playlist = {
+      id: Date.now().toString(), 
+      name,
+      description,
+      songs: [], 
+    };
+    setPlaylists((prevPlaylists) => [...prevPlaylists, newPlaylist]);
+    console.log('Nueva playlist creada:', newPlaylist); 
+  };
+
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Navbar />
-      <Container>
-        <h2>Nuestra Lista de Canciones</h2> 
-        <div
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap', 
-            justifyContent: 'center', 
-            gap: '20px', 
-            marginTop: '20px',
-          }}
-        >
-          {mockSongs.map((song) => (
-            <SongCard key={song.id} song={song} /> 
-          ))}
+      <div style={{ display: 'flex', flexGrow: 1 }}>
+        <Sidebar onAddPlaylist={addPlaylist} playlists={playlists} />
+        <div style={{ flexGrow: 1, overflowY: 'auto' }}>
+          <Container>
+            <h2>Nuestra Lista de Canciones</h2>
+            <div
+              style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'center',
+                gap: '20px',
+                marginTop: '20px',
+              }}
+            >
+              {mockSongs.map((song) => (
+                <SongCard key={song.id} song={song} />
+              ))}
+            </div>
+          </Container>
         </div>
-      </Container>
+      </div>
     </div>
   );
 }
